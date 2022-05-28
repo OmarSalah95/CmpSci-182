1eac54071e6a14d3401c452b8776d5c9bf0a71be
OmarSalah95 <omars.e.995@gmail.com>
2022-05-28 16:17:24 -0700

    Still having issues with the search algorithm, Rebuilt the add method as well as cleaned up the pre and post order displays. Bug in search, still looking for a solve

--- Project-5/src/phonebook/Main.java
+++ Project-5/src/phonebook/Main.java
@@ -4,13 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        PhoneBook pb1 = new PhoneBook(new Person("Omar Salah", "4704994575"));
-        pb1.add("A", "1234567891");
+        PhoneBook pb1 = new PhoneBook(new Person("Omar", "4704994575"));
         pb1.add("b", "1234567891");
+        pb1.add("A", "1234567891");
         pb1.add("c", "1234567891");
         pb1.add("y", "1234567891");
         pb1.add("x", "1234567891");
         pb1.add("z", "1234567891");
-        pb1.displayContacts();
+//        pb1.displayContacts();
+        pb1.displayPostorder();
+//        pb1.displayPreorder();
+        Person b = pb1.search("omar");
+        System.out.println("\n\n");
+        b.printInfo();
     }
 }
--- Project-5/src/phonebook/PhoneBook.java
+++ Project-5/src/phonebook/PhoneBook.java
@@ -1,5 +1,9 @@
 package phonebook;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.concurrent.TimeUnit;
+
 public class PhoneBook {
     private Person root;
 
@@ -16,43 +20,79 @@ public class PhoneBook {
 
 
     public void add(String nam, String num){
-        this.add(nam, num, this.root);
+        this.root = add(nam, num, this.root);
     }
 
-    public void add(String nam, String num, Person cur){
+    public Person add(String nam, String num, Person cur)  {
         Person newP;
 
-        if(nam.compareTo(cur.getName()) > 0){
-            if(cur.getRight() == null){
-                newP = new Person(nam,num);
-                cur.setRight(newP);
+            if (cur == null) {
+                cur = new Person(nam, num);
+                System.out.println("\n\n\n\nContact added\n\n\n\n");
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
                 clearScreen();
-                System.out.println("\n\n\n\n\n\n\nContact added");
-            } else {
-                add(nam, num, cur.getRight());
+                return cur;
             }
-        }else if(nam.compareTo(cur.getName()) < 0){
-            if(cur.getLeft() == null){
-                newP = new Person(nam,num);
-                cur.setLeft(newP);
-                System.out.println("\n\n\n\n\n\n\nContact added");
-            } else {
-                add(nam, num, cur.getRight());
-            }
-        } else {
-            System.out.println("This contact already exists, try updating instead!");
-        }
+
+            if (nam.compareToIgnoreCase(cur.getName()) < 0)
+                cur.setLeft(add(nam, num, cur.getLeft()));
+            else if (nam.compareToIgnoreCase(cur.getName()) > 0)
+                cur.setRight(add(nam,num, cur.getRight()));
+            return cur;
+
     }
 //    Delete
 //    Search
+    public Person search(String name){
+        return search(name, this.root);
+    }
+
+    public Person search(String name, Person cur) {
+        if (name.compareToIgnoreCase(cur.getName()) < 0 && cur.getLeft() != null)
+                 return search(name, cur.getLeft());
+        if (name.compareToIgnoreCase(cur.getName()) > 0 && cur.getRight() != null)
+                return  search(name, cur.getRight());
+        return cur;
+    }
+
     public void displayContacts(){
         displayContacts(this.root);
     }
+
     public void displayContacts(Person cur){
         if (cur == null) return;
             displayContacts(cur.getLeft());
             cur.printInfo();
             displayContacts(cur.getRight());
+
+    }
+    public void displayPreorder() {
+        displayPreorder(this.root);
+        System.out.println();
+    }
+
+    public void displayPreorder(Person cur) {
+        if (cur == null) return;
+
+        cur.printInfo();
+        displayPreorder(cur.getLeft());
+        displayPreorder(cur.getRight());
+
+    }
+
+    public void displayPostorder() {
+        displayPreorder(this.root);
+    }
+
+    public void displayPostorder(Person cur) {
+        if (cur == null) return;
+        displayPostorder(cur.getRight());
+        cur.printInfo();
+        displayPostorder(cur.getLeft());
 
     }
 }
